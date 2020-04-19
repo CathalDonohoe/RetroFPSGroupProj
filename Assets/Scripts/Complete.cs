8c2470a38c5b11d33c1bb48647fd4c05cac6d2ac
SourLeaf <cdonohoe1374@gmail.com>
2020-04-19 22:24:05 +0100

    Corrected enemy2 and added complete game UI

---
+++ Assets/Scripts/Complete.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Complete : MonoBehaviour
+{
+    public void Menu(){
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Quit(){
+        Application.Quit();
+    }
+}
